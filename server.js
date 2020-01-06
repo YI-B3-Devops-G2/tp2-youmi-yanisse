29038dbcbbc7b3ec2606e48a52fb2b93b219b3d9
Jérémy Marjollet <jeremy@marjollet.net>
2020-01-06 11:30:23 +0100

    Fix JS server

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '127.0.0.1';
+const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.json('Hello world\n');
+    res.json({
+        message: 'Hello World'
+    });
 });
 app.get('/status', (req, res) => {
-//
+    //
 });
 
 app.listen(PORT, HOST);
